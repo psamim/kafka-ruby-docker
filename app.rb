7a42ed79bee8e2ed4533188c7c86182ad25c4f77
Samim Pezeshki <p.samim@gmail.com>
2015-06-18 19:04:56 +0430

    Modify app to put requests into the queue

--- app.rb
+++ app.rb
@@ -1,39 +1,19 @@
-require 'poseidon'
-
-module Config
-  SERVER = 'kafka'
-  PORT = 9092
-  TOPIC = 'sol'
-end
+require 'producer'
 
 class App < Sinatra::Base
   enable :logging
+
+  def initialize(app = nil, params = {})
+    super(app)
+    @producer = Producer.new Config::REQUESTS_QUEUE
+  end
 
   get '/' do
     'Hello from docker!'
   end
 
   get '/sol/:sol' do
-    logger.info "Hello world."
-    "Hello #{params['sol']}!"
+    @producer.send params['sol']
+    "Request was put into queue for sol #{params['sol']}!"
   end
 end
-
-# class Consumer
-#   consumer = Poseidon::PartitionConsumer.new("my_test_consumer", Config::SERVER, Config::PORT, Config::TOPIC, 0, :earliest_offset)
-#   loop do
-#     messages = consumer.fetch
-#     messages.each do |m|
-#       puts m.value
-#     end
-#   end
-# end
-
-# class Producer
-#   producer = Poseidon::Producer.new(["#{Config::SERVER}:#{Config::PORT}"], "my_test_producer")
-
-#   messages = []
-#   messages << Poseidon::MessageToSend.new("topic1", "value1")
-#   messages << Poseidon::MessageToSend.new("topic2", "value2")
-#   producer.send_messages(messages)
-# end
