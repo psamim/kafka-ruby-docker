f09a76e9428a96d14c18967f9bc2074c24f7bc56
Samim Pezeshki <p.samim@gmail.com>
2015-06-19 15:27:35 +0430

    Produce messages only to partition one

--- kafka/producer.rb
+++ kafka/producer.rb
@@ -3,14 +3,20 @@ require 'config'
 
 class Producer
   def initialize(topic, id)
-    @producer = Poseidon::Producer.new ["#{Config::KAFKA_HOST}:#{Config::KAFKA_PORT}"], id
+
+    @producer = Poseidon::Producer.new ["#{Config::KAFKA_HOST}:#{Config::KAFKA_PORT}"], id, partitioner: partitioner
     @topic = topic
     @id = id
   end
 
   def send(value)
+    puts "Producer-ID #{@id}: Sending #{value.slice(0, 8)}... to topic #{@topic}"
     messages = []
     messages << Poseidon::MessageToSend.new(@topic, value, @id)
     @producer.send_messages(messages)
+  end
+
+  def partitioner
+    Proc.new { |key, partition_count| 1 }
   end
 end
