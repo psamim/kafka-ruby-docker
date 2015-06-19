83ef3edc0f3478d2078b3cc46c7724414a995e86
Samim Pezeshki <p.samim@gmail.com>
2015-06-19 19:17:06 +0430

    Remove unneeded configs

--- config.rb
+++ config.rb
@@ -3,8 +3,6 @@ module Config
   KAFKA_PORT = ENV['KAFKA_PORT_9092_TCP_PORT']
   ZK_HOST = ENV['ZK_PORT_2181_TCP_ADDR']
   ZK_PORT = ENV['ZK_PORT_2181_TCP_PORT']
-  PRO_ID = 'pro_id'
-  CONS_ID = 'cons_id'
   REQUESTS_QUEUE = 'reqs'
   SOL_TOPIC = 'sol'
 end
