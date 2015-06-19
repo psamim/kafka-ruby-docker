9c504a109d820b306e9028fabbaaa45cce791543
Samim Pezeshki <p.samim@gmail.com>
2015-06-19 07:56:29 +0430

    Try kafka-rb instead of poseidon

--- kafka/consumer.rb
+++ kafka/consumer.rb
@@ -1,25 +1,47 @@
-require 'poseidon'
+# require 'poseidon'
 require 'config'
+require 'kafka'
 
 class Consumer
   def initialize(topic)
-    @consumer = Poseidon::PartitionConsumer.new(Config::CONS_ID, Config::SERVER, Config::PORT, topic, 0, :earliest_offset)
+    # kafka-rb
+    @consumer = Kafka::Consumer.new host: Config::SERVER, port: Config::PORT, topic: topic, polling: 1
+
+    # poseidon
+    # @consumer = Poseidon::PartitionConsumer.new(Config::CONS_ID, Config::SERVER, Config::PORT, topic, 0, :earliest_offset)
+
     @topic = topic
   end
 
   def listen
-    loop do
-      begin
-        messages = @consumer.fetch
-        messages.each do |m|
-          yield(m)
-        end
-      rescue Exception => e
-        puts e.message
-        # puts e.backtrace.inspect
-        puts "Trying in 5 seconds"
-        sleep 5
+    # kafka-rb
+    begin
+      consumer.loop do |messages|
+        puts "Received"
+        puts messages
+        yield(messages)
       end
+    rescue Exception => e
+      puts e.message
+      # puts e.backtrace.inspect
+      puts "Trying in 5 seconds"
     end
+
+    # poseidon
+    # loop do
+    #   begin
+    #     messages = @consumer.fetch
+    #     messages.each do |m|
+    #       puts m.value
+    #       puts m.offset
+    #       yield(m)
+    #     end
+    #   rescue Exception => e
+    #     puts e.message
+    #     # puts e.backtrace.inspect
+    #     puts "Trying in 5 seconds"
+    #     sleep 5
+    #   end
+    # end
   end
 end
--- kafka/producer.rb
+++ kafka/producer.rb
@@ -1,15 +1,26 @@
-require 'poseidon'
+# require 'poseidon'
+require 'kafka'
 require 'config'
 
 class Producer
   def initialize(topic)
-    @producer = Poseidon::Producer.new(["#{Config::SERVER}:#{Config::PORT}"], Config::PRO_ID, :type => :sync)
+    # kafka-rb
+    @producer = Kafka::Producer.new :host => Config::SERVER, :port => Config::PORT, :topic => topic, :polling => 1
+
+    # poseidon
+    # @producer = Poseidon::Producer.new(["#{Config::SERVER}:#{Config::PORT}"], Config::PRO_ID, :type => :sync)
     @topic = topic
   end
 
   def send(value)
-    messages = []
-    messages << Poseidon::MessageToSend.new(@topic, value)
-    @producer.send_messages(messages)
+    # kafka-rb
+    puts "Sending value #{value} to topic #{@topic}."
+    message = Kafka::Message.new(value)
+    @producer.push(message)
+
+    # poseidon
+    # messages = []
+    # messages << Poseidon::MessageToSend.new(@topic, value)
+    # @producer.send_messages(messages)
   end
 end
